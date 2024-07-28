175a26662481a8210419a59c44b438f754f7bb58
Ky Huynh <kyhuynh@kms-technology.com>
2024-07-28 14:58:42 +0700

    Sample using external modules

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kyhuynhkms/Golang-my-project
+
+go 1.22.2
+
+require github.com/kyhuynhkms/Golang-my-module v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package golangmyproject
+
+import (
+	"fmt"
+
+	golangmymodule "github.com/kyhuynhkms/Golang-my-module"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println(golangmymodule.Add(1, 2))
+}
